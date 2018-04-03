0468bee001517d4d1c17424ec3b0553f1edca8ca
Sina Bakhtiari <sinabakh44@gmail.com>
2018-04-03 18:42:03 +0430

    Add Table Existence Checker And Creator To DB Init Method

--- bot/utils/DB.py
+++ bot/utils/DB.py
@@ -2,7 +2,15 @@ from peewee import *
 from . import Configs
 
 # Instance of the database (MySQL)
-db = MySQLDatabase(Configs.DB.NAME, **{'host': Configs.DB.HOST, 'port': Configs.DB.PORT, 'user': Configs.DB.USER})
+db = MySQLDatabase(
+    Configs.DB.NAME,
+    **{
+        'host': Configs.DB.HOST,
+        'port': Configs.DB.PORT,
+        'user': Configs.DB.USER,
+        'password': Configs.DB.PASSWORD
+    }
+)
 
 
 # Base model for all models
@@ -21,6 +29,10 @@ class Client(BaseModel):
         table_name = Configs.DB.CLIENTS_TABLE
 
 
+# Initialize database and dao
 def init():
+    # Connect to database
     db.connect()
+    # Create tables if they don't exist
+    db.create_tables([Client])
 
